f3807cad623d7751b774ce68ddd97466e239f4f6
Deon Holo <81697769+DeonHolo@users.noreply.github.com>
2026-04-21 21:08:29 +0800

    feat: deleted sys_properties from repo

--- src/fluent/generated/properties/system-property/sys_properties_fda673ba0fdcc35032c14bc530d1b28b.now.ts
+++
@@ -1,9 +0,0 @@
-import { Property } from '@servicenow/sdk/core'
-
-Property({
-    $id: Now.ID['fda673ba0fdcc35032c14bc530d1b28b'],
-    name: 'x_1978345_flowdesk.gemini_api_key',
-    value: 'AIzaSyDZ87lw4m-FERfagyqb7rQ6eTzutg0RTbE',
-    description: 'Google Gemini API key for FlowDesk AI features',
-    ignoreCache: true,
-})
